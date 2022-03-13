c1ce853cdbac18373a760d5890bc82711dd44c41
Loke Sankrandan <lokesankrandan@users.noreply.github.com>
2022-03-12 19:10:34 -0600

    Login form html

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import LoginForm from './LoginForm';
 
 class App extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Hello World!</h1>
-            </div>
+            // <div>
+            //     <h1>Hello World!</h1>
+            // </div>
+            <LoginForm />
         );
     }
 
---
+++ src/components/LoginForm.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import axios from 'axios';
+// import "./App.css";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return username.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    axios.post(`http://localhost:8000/auth/login`, { username, password })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            autoFocus
+            type="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+}
