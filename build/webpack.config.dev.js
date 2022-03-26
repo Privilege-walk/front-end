32dbae91c319d0e260a93c903f2010a103fc9467
jnyangwe <josephocharo7@gmail.com>
2022-03-26 14:18:59 -0500

    webpack config

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+
+module.exports = {
+
+    entry: "./src/index.js",
+
+    output: {
+        path: path.join(__dirname, '/build'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './public/index.html'
+        }),
+        new webpack.DefinePlugin({
+            'process.env.BASE_URL': JSON.stringify('http://127.0.0.1:8000')
+        }),
+    ],
+
+    devServer: {
+        allowedHosts: 'all',
+        historyApiFallback: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  'style-loader',
+                  'css-loader'
+                ]
+            }
+        ]
+    }
+
+}
